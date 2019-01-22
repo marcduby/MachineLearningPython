6b896f180c2f3518be9e1b0efd8fe5ac496bf8e7
mduby <mduby@broadinstitute.org>
2019-01-21 22:17:45 -0500

    working workout xski plot

--- Introductions/Scratch/workoutSki.py
+++ Introductions/Scratch/workoutSki.py
@@ -6,11 +6,12 @@ from mysql.connector import errorcode
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-
+# main variables
+x = []
+y = []
 
 # connect to the db
 # client = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="yoyoma", db="workout_test")
-
 try:
     connection = mysql.connector.connect(
         user = 'root',
@@ -35,7 +36,7 @@ try:
     # build the query
     query = """
     select sum(activity.x_ski) as xski_total, count(1) as ski_workouts, sum(activity.x_ski)/count(1) as workout_avg,
-    count(distinct wday.day_id) as ski_days, sum(activity.x_ski)/count(distinct wday.day_id) as day_avg, wyear.name_text
+    count(distinct wday.day_id) as ski_days, sum(activity.x_ski)/count(distinct wday.day_id) as day_avg, wyear.name
 from wkt_workout workout, wkt_activity activity, wkt_day wday, wkt_week week, wkt_period period, wkt_year wyear
 where workout.day_id = wday.day_id
     and activity.workout_id = workout.workout_id
@@ -44,7 +45,7 @@ where workout.day_id = wday.day_id
     and period.year_id = wyear.year_id
     and activity.x_ski > 0
 group by wyear.name_text
-order by xski_total desc, ski_workouts desc
+order by wyear.name
     """
 
     # query = "select sum(activity.x_ski) as xski_total, count(1) as ski_workouts from wkt_activity activity"
@@ -64,9 +65,17 @@ order by xski_total desc, ski_workouts desc
     # loop though the rows
     for row in results:
         xski_total = row[0]
+        y.append(xski_total)
+
+        # get number workouts
         xski_workout = row[1]
 
-        print "kms: {}, workouts {}".format(xski_total, xski_workout)
+        # get year names
+        year_name = row[5]
+        x.append(year_name)
+
+        # log
+        print "kms: {}, workouts {}, year name {}".format(xski_total, xski_workout, year_name)
 
 
 except mysql.connector.Error as err:
@@ -76,6 +85,21 @@ finally:
     cursor.close()
     connection.close()
 
+# plot
+# plt.plot(x, y)
+plt.bar(x, y)
+plt.xticks(x)
+plt.axes().yaxis.grid()
+
+# log
+print('plot created')
+
+# set options
+# plt.interactive(False)
+
+# show the plot
+plt.show()
+print('show plot')
 
 
 
