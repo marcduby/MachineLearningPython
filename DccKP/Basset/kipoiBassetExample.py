5ca7ff872c902ac272ea08d8862092db9ccdc4c1
Marc Duby <marc_duby@post.harvard.edu>
2020-07-09 00:06:11 -0400

    Basset: working Basset predictions

--- DccKP/Basset/kipoiBassetExample.py
+++ DccKP/Basset/kipoiBassetExample.py
@@ -7,10 +7,14 @@ import pybedtools
 # load the model
 model = kipoi.get_model('Basset')
 
+print(model.model)
+
 # make the prediction
 prediction = model.pipeline.predict_example()
 
 # print
 print("the prediction of type {} and shape {} is \n{}".format(type(prediction), prediction.shape, prediction))
+
+print(model.default_dataloader)
 
 
