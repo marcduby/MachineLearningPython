6d1faf12eddcaf7de1bd45363981eedc0d23c77c
Marc Duby <mduby@broadinstitute.org>
2020-07-20 15:58:32 -0400

    Basset: added translation script

--- DccKP/Basset/Pytorch/testNasaModel.py
+++ DccKP/Basset/Pytorch/testNasaModel.py
@@ -9,10 +9,20 @@ import csv
 
 # file locations
 dir_data = "/Users/mduby/"
-file_model = dir_data + "Data/Broad/Basset/Nasa/ampt2d_cnn_900_best.th"
+dir_code = "/Users/mduby/Code/WorkspacePython/"
+file_model_weights = dir_data + "Data/Broad/Basset/Nasa/ampt2d_cnn_900_best.th"
+
+# import relative libraries
+import sys
+sys.path.insert(0, dir_code + 'MachineLearningPython/DccKP/Basset/')
+import dcc_basset_lib
+
 
 # load the model
-model = torch.load(file_model)
+# model = torch.load(file_model_weights)
+
+# load the model using the library
+pretrained_model_reloaded_th = dcc_basset_lib.load_basset_model(file_model_weights)
 
 # print the model
 print("the model is \n{}".format(model))
