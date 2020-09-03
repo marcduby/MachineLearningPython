c0148e0821c5ca8b0a078013e12a9b5bf2b7b8e2
Marc Duby <marc_duby@post.harvard.edu>
2020-09-02 23:45:50 -0400

    Ensemble: added max voting

--- EnsembleLearning/MaxVoting/maxVotingSkLearn.py
+++ EnsembleLearning/MaxVoting/maxVotingSkLearn.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
 
 print("the version of sklearn is {}".format(sklearn.__version__))
 print("the version of numpy is {}".format(np.__version__))
@@ -48,14 +49,35 @@ print("the score for the new knn model is {}\n".format(knn_score2))
 # random forest classifier
 rf = RandomForestClassifier(random_state = 3)
 rf_params_search = {'n_estimators': [50, 100, 150, 200]}
-rf_grid = GridSearchCV(rf, rf_params_search, cv = 5)
+rf_grid = GridSearchCV(rf, rf_params_search, cv = 5, verbose=1)
 rf_grid.fit(X_train, y_train)
 rf_best = rf_grid.best_estimator_
 print("the new best random forest model is {}".format(rf_best.get_params()))
 rf_score = rf_best.score(X_test, y_test)
-print("the score for the random forest model is {}".format(rf_score))
+print("the score for the random forest model is {}\n".format(rf_score))
 
+# logistic regression
+lr = LogisticRegression(random_state = 3, solver='liblinear', penalty='l2', max_iter=5000)
+C = np.logspace(1, 4, 7, 10)
+# solver_list = ['newton-cg', 'liblinear', 'sag', 'saga']
+solver_list = ['newton-cg', 'liblinear']
+lr_params_search = {'C': C, 'solver': solver_list}
+lr_grid = GridSearchCV(lr, lr_params_search, cv = 5, verbose=1)
+lr_grid.fit(X_train, y_train)
+lr_best = lr_grid.best_estimator_
+print("the new best logistic regression model is {}".format(lr_best.get_params()))
+lr_score = lr_best.score(X_test, y_test)
+print("the score for the logistic regression model is {}\n".format(lr_score))
 
+# build the ensemble estimator
+estimators = [('knn', knn_best), ('rf', rf_best), ('lr', lr_best)]
+ensemble_max_voting = VotingClassifier(estimators, voting = 'soft')
+ensemble_max_voting.fit(X_train, y_train)
+ensemble_score = ensemble_max_voting.score(X_test, y_test)
+print("the score for the knn model is {}\n".format(knn_score))
+print("the score for the random forest model is {}\n".format(rf_score))
+print("the score for the logistic regression model is {}\n".format(lr_score))
+print("the score for the ensemble max voting model is {}\n".format(ensemble_score))
 
 
 
