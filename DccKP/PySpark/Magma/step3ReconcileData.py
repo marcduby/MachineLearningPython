a9b689843abc1405eb4ffd32bf18dbaf5890fabd
Marc Duby <marc_duby@post.harvard.edu>
2020-09-13 20:18:45 -0400

    Magma: working last step

--- DccKP/PySpark/Magma/step3ReconcileData.py
+++ DccKP/PySpark/Magma/step3ReconcileData.py
@@ -34,7 +34,7 @@ out_dir = '/home/javaprog/Data/Broad/dig-analysis-data/out/magma/results'
 
 # ncbi schema
 ncbi_schema = StructType([
-    StructField("geneId", IntegerType(), True),
+    StructField("geneNcbiId", IntegerType(), True),
     StructField("chromosome", StringType(), True),
     StructField("start", IntegerType(), True),
     StructField("end", IntegerType(), True),
@@ -88,7 +88,7 @@ def load_gene_pvalues(input_dir, phenotype):
 
     # now split the columns
     split_col = split(df_load.generic, '\\s+',)
-    df = df_load.withColumn("geneId", split_col.getItem(0))\
+    df = df_load.withColumn("geneNcbiId", split_col.getItem(0))\
         .withColumn("nParam", split_col.getItem(5))\
         .withColumn("subjects", split_col.getItem(6))\
         .withColumn("zStat", split_col.getItem(7))\
@@ -97,46 +97,50 @@ def load_gene_pvalues(input_dir, phenotype):
     # return
     return df
 
-
-
-# load the lookup file
-# df_ncbi = load_ncbi_lookup(ncbi_srcdir)
-# df_ncbi.show()
-
-# # load the gene pvalue file
-# df_pvalue = load_gene_pvalues(gene_pvalues_srcdir, phenotype)
-# df_pvalue.show()
+# load the gene pvalue file
+df_pvalue = load_gene_pvalues(gene_pvalues_srcdir, phenotype)
+df_pvalue.show()
 
 # load the ontology file# load the ontology file
-
-
 df_ontology = load_phenotype_ontology_lookup(ncbi_srcdir)
 df_ontology = df_ontology \
     .select('PH', 'MONDO ID exact', 'EFO ID exact') \
-    .withColumnRenamed('PH', 'phenotype') \
     .withColumnRenamed('MONDO ID exact', 'phenotypeMondoId') \
     .withColumnRenamed('EFO ID exact', 'phenotypeEfoId') 
 df_ontology.show()
 
-# join the two dataframes and add in rsIDs
-# df_export = df_pvalue.join(df_ncbi, on='geneId', how='inner')
-# df_export = df_export.select('gene', 'geneId', 'nParam', 'subjects', 'zStat', 'pValue')\
-#     .withColumn('geneId', df_export['geneId'].cast(IntegerType()))\
-#     .withColumn('phenotype', lit(phenotype))\
-#     .withColumn('zStat', df_export['zStat'].cast(DoubleType()))\
-#     .withColumn('subjects', df_export['subjects'].cast(IntegerType()))\
-#     .withColumn('nParam', df_export['nParam'].cast(IntegerType()))\
-#     .withColumn('pValue', df_export['pValue'].cast(DoubleType()))
-# print("the loaded variant joined data frame has {} rows".format(df_export.count()))
-# df_export.show()
+# filter the ontology by phenotype
+df_ontology = df_ontology.filter(df_ontology.PH == phenotype)
+df_ontology.show()
 
-# # write out the one tab delimited file
-# df_export \
-#         .orderBy(df_export.gene) \
-#         .write \
-#         .mode('overwrite') \
-#         .json('%s/%s' % (out_dir, phenotype))
-# print("wrote out the reconciled gene pvalue data frame to directory {}/{}".format(out_dir, phenotype))
+# join the ontology with the gene values
+df_phenotype_pvalue = df_pvalue.join(df_ontology, how='outer')
+df_phenotype_pvalue.show()
+
+# load the lookup file
+df_ncbi = load_ncbi_lookup(ncbi_srcdir)
+df_ncbi.show()
+
+# join the two dataframes and add in rsIDs
+df_export = df_phenotype_pvalue.join(df_ncbi, on='geneNcbiId', how='inner')
+df_export.show()
+df_export = df_export.select('gene', 'geneNcbiId', 'nParam', 'subjects', 'zStat', 'pValue', 'phenotypeEfoId', 'phenotypeMondoId') \
+    .withColumn('geneNcbiId', df_export['geneNcbiId'].cast(IntegerType())) \
+    .withColumn('phenotype', lit(phenotype))\
+    .withColumn('zStat', df_export['zStat'].cast(DoubleType())) \
+    .withColumn('subjects', df_export['subjects'].cast(IntegerType())) \
+    .withColumn('nParam', df_export['nParam'].cast(IntegerType())) \
+    .withColumn('pValue', df_export['pValue'].cast(DoubleType()))
+print("the loaded variant joined data frame has {} rows".format(df_export.count()))
+df_export.show()
+
+# write out the one tab delimited file
+df_export \
+        .orderBy(df_export.gene) \
+        .write \
+        .mode('overwrite') \
+        .json('%s/%s' % (out_dir, phenotype))
+print("wrote out the reconciled gene pvalue data frame to directory {}/{}".format(out_dir, phenotype))
 
 # stop spark
 # spark.stop()
