fd5bbec0a22d4c9780ae2789987e360924de9c79
Marc Duby <mduby@broadinstitute.org>
2021-07-20 16:00:48 -0400

    Molepro: added query reversing util

--- DccKP/Basset/Data/webScrapeBassetLabels.py
+++ DccKP/Basset/Data/webScrapeBassetLabels.py
@@ -4,15 +4,18 @@
 # http://genome.ucsc.edu/cgi-bin/hgEncodeVocab?ra=encode/cv.ra&type=Cell+Line&tier=2&bgcolor=FFFEE8
 # http://genome.ucsc.edu/cgi-bin/hgEncodeVocab?ra=encode/cv.ra&type=Cell+Line&tier=3&bgcolor=FFFEE8
 
+# reference code from video https://www.youtube.com/watch?v=15f4JhJ8SiQ&t=505s
 
 # imports
 import pandas as pd 
 import requests 
 from bs4 import BeautifulSoup
+import json 
 
 # constants
 url_type01 = 'http://genome.ucsc.edu/cgi-bin/hgEncodeVocab?ra=encode/cv.ra&type=Cell+Line&tier=1&bgcolor=FFFEE8'
 file_bassett_labels = "/home/javaprog/Data/OldNUC/Broad/Basset/Production/basset_labels.txt"
+file_bassett_labels_out = "/home/javaprog/Data/OldNUC/Broad/Basset/Production/basset_labels_with_text.json"
 
 def get_tissue_array_data(url):
     ''' takes in url, returns tissue array df of results of table '''
@@ -44,6 +47,37 @@ def get_tissue_array_data(url):
     print("the temp data is of size {}".format(len(results)))
     return results
 
+def get_tissue_map_data(url):
+    ''' takes in url, returns tissue array df of results of table '''
+    # initialize
+    results = []
+
+    # get the request
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    # get data
+    table_data = soup.find('table', class_='sortable')
+    print()
+    # print(table_data)
+
+    for body in table_data.find_all('tbody'):
+        rows = body.find_all('tr')
+        for row in rows:
+            new_tissue = {'cellCode': row.find_all('td')[0].text, 
+                            'cellDescription': row.find_all('td')[2].text,
+                            'lineage': row.find_all('td')[3].text.strip() if len(row.find_all('td')[3].text.strip()) > 0 else None, 
+                            'tissue': row.find_all('td')[4].text.strip() if len(row.find_all('td')[4].text.strip()) > 0 else None, 
+                            'karyotype': row.find_all('td')[5].text.strip() if len(row.find_all('td')[5].text.strip()) > 0 else None}
+            
+            # print(new_tissue)
+            results.append(new_tissue)
+
+    # return
+    print("the temp data is of size {}".format(len(results)))
+    return results
+
+
 if __name__ == '__main__':
     # initialize
     array_tissues = []
@@ -57,27 +91,41 @@ if __name__ == '__main__':
 
 # loop
 for url in array_url:
-    array_tissues += get_tissue_array_data(url)
+    array_tissues += get_tissue_map_data(url)
     print("the tissue array is of size {}".format(len(array_tissues)))
 
 # create a pandas dataframe
-df_tissues = pd.DataFrame(array_tissues, columns=['cell_code', 'cell_description', 'lineage', 'tissue', 'karyotype'])
+df_tissues = pd.DataFrame(array_tissues, columns=['cellCode', 'cellDescription', 'lineage', 'tissue', 'karyotype'])
 print(df_tissues.info())
 print(df_tissues.head(10))
 
 # read the data file
-df_bassett_labels = pd.read_csv(file_bassett_labels, names=['cell_code'])
+df_bassett_labels = pd.read_csv(file_bassett_labels, names=['cellCode'])
 
 # print
 print()
 print(df_bassett_labels.info())
 print()
 print(df_bassett_labels.head(10))
+list_labels = df_bassett_labels['cellCode'].tolist()
+print("got labels list of size {}".format(len(list_labels)))
 
-# inner join the two cell label data 
-df_joined = pd.merge(df_bassett_labels, df_tissues, on='cell_code')
+# # inner join the two cell label data 
+# df_joined = pd.merge(df_bassett_labels, df_tissues, on='cell_code')
+# print()
+# print(df_joined.info())
+# print()
+# print(df_joined.head(10))
+
+# create new array of tissues
+list_final_tissues = [tissue for tissue in array_tissues if tissue.get('cellCode') in list_labels]
 print()
-print(df_joined.info())
-print()
-print(df_joined.head(10))
+print("final size of joined labels is {}".format(len(list_final_tissues)))
+
+# write out the results
+with open(file_bassett_labels_out, 'w') as out_file:
+    for row in list_final_tissues:
+        out = json.dumps(row, separators=(',', ':'))
+        out_file.write(out)
+        out_file.write('\n')
 
--- DccKP/Translator/MolecularProvider/Utils/objectIdReverse.py
+++ DccKP/Translator/MolecularProvider/Utils/objectIdReverse.py
@@ -1,5 +1,64 @@
 
 
 # imports
+import copy
+
+# constants
+MESSAGE = "message"
+QUERY_GRAPH = "query_graph"
+EDGES = "edges"
+SUBJECT = "subject"
+OBJECT = "object"
+PREDICATES = "predicates"
+NODES = "nodes"
+IDS = "ids"
+CATEGORIES = "categories"
 
 
+# methods
+def reverse_query(query, map_predicate, debug=False):
+    ''' will check to see if query has id on object id only, then flips it if applicable; do nothing if not '''
+    #initialize
+    result = None
+    subject = None
+    object = None
+    query_graph = None
+    edge_id = None
+    predicates = None
+
+    # get the data
+    if query.get(MESSAGE).get(QUERY_GRAPH):
+        query_graph = query.get(MESSAGE).get(QUERY_GRAPH)
+        if query_graph.get(EDGES):
+            edges = query.get(MESSAGE).get(QUERY_GRAPH).get(EDGES)
+            if len(edges) == 1:
+                for key, value in edges:
+                    edge_id = key
+                    subject = edges.get(SUBJECT)
+                    object = edges.get(OBJECT)
+                    predicates = edges.get(PREDICATES)
+
+                # test if id on object only
+                if subject and object:
+                    if query_graph.get(NODES).get(object).get(IDS) and not query_graph.get(NODES).get(subject).get(IDS):
+                        # copy and flip
+                        result = copy.deepcopy(query)
+
+                        # flip
+                        result.get(MESSAGE).get(QUERY_GRAPH).get(EDGES).get(edge_id).get(SUBJECT) = object
+                        result.get(MESSAGE).get(QUERY_GRAPH).get(EDGES).get(edge_id).get(OBJECT) = subject
+                        predicates_reverse = []
+                        for item in predicates:
+                            predicates_reverse.add(map_predicate.get(item))
+                                    
+    # return orginal and result
+    return query, result
+
+
+# main
+if __name__ == "__main__":
+    # load the input file
+
+    # reverse
+
+    # test
