f4d21e6a29f1acb4a3a2668c40722c6468f54f12
Marc Duby <mduby@broadinstitute.org>
2023-05-03 14:08:13 -0400

    gpt: fixed model saving location bug

--- DccKP/GPT/Pubmed/TextGeneration/trainPubmedTextGenerationGPT.py
+++ DccKP/GPT/Pubmed/TextGeneration/trainPubmedTextGenerationGPT.py
@@ -93,9 +93,9 @@ def train(chatData, model, optim, num_epochs=25):
         if i % ML_INTERVAL_SAVE_MODEL == 0:
             # file_model = "{}/text_gen_model_state_{}.pt".format(DIR_MODEL, i)
             # torch.save(model.state_dict(), file_model)
-            dir_temp = DIR_MODEL + "/text_gen_{}".format(i)
+            dir_temp = DIR_MODEL + "/Text_gen_{}".format(i)
             os.mkdir(dir_temp)
-            model.save_pretrained(DIR_MODEL)
+            model.save_pretrained(dir_temp)
             print("wrote out model for epoch: {} to file: {}".format(i, dir_temp))
 
         # test the inference
